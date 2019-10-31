fd9bb60867ec72fb7576800bf242cd676ba7b8d6
Michael Kolomiets <michael.kolomiets@gmail.com>
2019-10-31 12:34:14 +0200

    RTT Logger

--- main.c
+++ main.c
@@ -49,9 +49,11 @@
 #include "nrf.h"
 #include "nordic_common.h"
 #include "boards.h"
+
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
+
 #include "app_scheduler.h"
 
 /**
@@ -62,6 +64,10 @@ int main(void)
     char string_on_stack[] = "stack";
     uint32_t err_code;
     err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+
     NRF_LOG_INFO("%s", NRF_LOG_PUSH(string_on_stack));
     NRF_LOG_FLUSH();
     while (true)
